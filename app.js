079411d04e3b785ce0a1f20536a99cf10f6615d1
MMcPeek <54789447+MMcPeek@users.noreply.github.com>
2024-02-14 11:14:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,31 @@ angular.module('LunchCheck', [])
 
 .controller('LunchCheckController', function ($scope) {
   $scope.lunches = "";
-  const str = "";
-  const strCopy = str.split();
-  $scope.lunches = strCopy;
+  $scope.message = "";
   
   $scope.yesButton = function() {
 	$scope.isSelected = true;
-	$scope.formData.yesPool = true;
-	}
-}
+	
+	if($scope.lunches !== "" && $scope.lunches !== null && $scope.lunches !== undefined) {
+		function splitString(stringToSplit, separator) {
+			const arrayOfStrings = stringToSplit.split(separator);
+			var strLength = arrayOfStrings.length;
+	
+			if (strLength < 4) {
+				$scope.message = "Enjoy!"
+			};
+			if (strLength > 3) {
+				$scope.message = "Too much!"
+			};
+		};
+  
+	const comma = ",";
+	splitString($scope.lunches, comma);
+	
+	} else {
+	  $scope.message = "Please enter data first";
+	};
+  };
+});
+}) ();
+
